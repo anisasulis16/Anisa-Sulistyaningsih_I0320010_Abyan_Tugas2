7b0f5086cad21ae8c1c7761ca2e8550d392df2b4
Anisa.S <Anisa.Sulistyaningsih16@gmail.com>
2021-03-12 16:51:50 +0700

    Soal2

--- I0320010_soal2_tugas2.py
+++ I0320010_soal2_tugas2.py
@@ -1,3 +1,5 @@
+#Menampilkan informasi program
+print("Program Identitas")
 
 #Menginputkan variabel nama
 nama = input("Masukkan Nama Anda: ")
@@ -10,6 +12,18 @@ bulan_sekarang = int(input("Masukkan Bulan Sekarang: "))
 tahun_lahir = int(input("Masukkan Tahun Lahir: "))
 tahun_sekarang = int(input("Masukkan Tahun Saat ini: "))
 
+#Menginputkan tinggi badan
+tinggi_badan = float(input("Masukkan Tinggi Badan Anda: "))
+
+#Menginputkan ukuran sepatu
+ukuran_sepatu = float(input("Masukkan Ukuran Sepatu: "))
+
+#Menginputkan alamat rumah
+alamat_rumah = input("Masukkan Alamat Rumah Anda: ")
+
+#Menginputkan pekerjaan
+pekerjaan = input("Pekerjaan: ")
+
 #Menghitung umur
 kelahiran = tanggal_lahir + (bulan_lahir * 30) + (tahun_lahir * 365)
 saat_ini = tanggal_sekarang + (bulan_sekarang * 30) + (tahun_sekarang * 365)
@@ -18,8 +32,13 @@ tahun = (saat_ini - kelahiran) / 365
 bulan = (tahun - int(tahun)) * 12
 hari = (bulan - int(bulan)) * 30
 
-#Menampilkan hasi proses
 print(".............................")
+#Menampilkan hasil proses
+print("identitas Pribadi")
 print("Nama: ", nama)
 print("Anda berumur: ", int(tahun), "tahun", int(bulan), "bulan", int(hari), "hari")
-
+print("Tinggi badan: ", tinggi_badan)
+print("Ukuran Sepatu: ", ukuran_sepatu)
+print("Alamat rumah: ", alamat_rumah)
+print("Pekerjaan: ", pekerjaan)
+print(".............................")
